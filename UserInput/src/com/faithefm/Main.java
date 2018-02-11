836082e0662de047d28c537abaa4266d703869e5
faithfem <fmugure@intracitygeeks.org>
2018-02-10 20:42:11 -0500

    UserInput

--- UserInput/src/com/faithefm/Main.java
+++ UserInput/src/com/faithefm/Main.java
@@ -2,8 +2,8 @@ package com.faithefm;
 import java.util.Scanner;
 
 public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String []args){
+        Scanner faith = new Scanner(System.in);
+        System.out.println(faith.nextLine());
     }
 }
--- UserInput/src/com/faithefm/apples.java
+++
@@ -1,9 +0,0 @@
-package com.faithefm;
-import java.util.Scanner;
-
-public class apples {
-    public static void  main (String[]args){
-        Scanner faith = new Scanner(System.in);
-    }
-
-}
