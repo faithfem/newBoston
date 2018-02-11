28cbb3d4527c23d852702e379ae61a96c3ed4194
faithfem <fmugure@intracitygeeks.org>
2018-02-10 20:36:21 -0500

    Calculator

---
+++ UserInput/src/com/faithefm/Main.java
@@ -0,0 +1,9 @@
+package com.faithefm;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ UserInput/src/com/faithefm/apples.java
@@ -0,0 +1,9 @@
+package com.faithefm;
+import java.util.Scanner;
+
+public class apples {
+    public static void  main (String[]args){
+        Scanner faith = new Scanner(System.in);
+    }
+
+}
