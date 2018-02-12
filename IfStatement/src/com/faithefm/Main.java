1f7e41c55007680f875aa540e0dcf6801c410863
faithfem <fmugure@intracitygeeks.org>
2018-02-11 23:11:36 -0500

    Operatros, IfStatement and Switch

---
+++ IfStatement/src/com/faithefm/Main.java
@@ -0,0 +1,52 @@
+package com.faithefm;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Variables
+        int test = 6;
+
+        if (test <= 9){
+            System.out.println("test is less than or equal to 9");
+        } else{
+            System.out.println("test is not equal to 9");
+        }
+
+        //Operators
+        int boy, girl;
+        boy = 9;
+        girl = 70;
+        if(boy > 10 || girl <60){
+            System.out.println("You can enter");
+        } else{
+            System.out.println("You cannot enter.");
+        }
+
+        //Switch Statemeent
+        //Variables
+
+        int age;
+        age = 10;
+
+        switch (age){
+            case 1:
+                System.out.println("You can crawl");
+                break;
+
+            case 2:
+                System.out.println("You can talk");
+                break;
+
+            case 3:
+                System.out.println("You can get in trouble");
+                break;
+
+                default:
+                    System.out.println("You are not age " + age);
+        }
+
+    }
+
+
+}
