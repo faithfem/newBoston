8a86d406ff11f260093ae95e31c8beb0701a7e01
faithfem <fmugure@intracitygeeks.org>
2018-03-03 14:20:51 -0500

    Math formulars

---
+++ MathClassMethods/src/tuna.java
@@ -0,0 +1,15 @@
+public class tuna {
+
+    public static void main(String[]args){
+        System.out.println(Math.abs(-26.7));
+        System.out.println(Math.ceil(7.7));
+        System.out.println(Math.floor(7.4));
+        System.out.println(Math.max(8.6, 5.2));
+        System.out.println(Math.min(8.6, 5.2));
+        System.out.println(Math.pow(3, 2)); //3 to the power of 2
+        System.out.println(Math.sqrt(4));
+
+
+
+    }
+}
