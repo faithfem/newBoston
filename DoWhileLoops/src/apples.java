14227af9170fbc8c290773ed0c7af3a2f1425db3
faithfem <fmugure@intracitygeeks.org>
2018-03-03 13:51:58 -0500

    Do While Loop

---
+++ DoWhileLoops/src/apples.java
@@ -0,0 +1,15 @@
+public class apples {
+    public static void main(String[]args){
+
+        //declare variables
+
+        int counter = 15;
+
+        do{
+            //you want to print while...
+            System.out.println(counter);
+            counter++;
+        } while (counter<=10);
+
+    }
+}
