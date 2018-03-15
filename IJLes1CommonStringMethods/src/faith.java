624c1ba8d8baffde34b159b5513b9fee1cc34d00
faithfem <fmugure@intracitygeeks.org>
2018-03-15 16:55:58 -0400

    strings

---
+++ IJLes1CommonStringMethods/src/faith.java
@@ -0,0 +1,14 @@
+public class faith {
+
+    public static void main(String[]args){
+
+        String[] myString = {"funk", "chunk","furry", "bacon"};
+
+        //find words that start with fu
+        //WHY DECLARE ANOTHER VARIABLE IN THE FOR LOOP?
+            for (String x : myString) { //Why declare another variable x
+                if (x.startsWith("fu"))
+                    System.out.println(x + " starts with fu");
+            }
+    }
+}
