44b899ca0d1eb69392e56c1cab47640548a9e4a9
faithfem <fmugure@intracitygeeks.org>
2018-03-16 13:05:50 -0400

    Interm Java

--- IJLes1CommonStringMethods/src/Dog.java
+++ IJLes1CommonStringMethods/src/Dog.java
@@ -8,31 +8,37 @@ public class Dog {
             // a constructor is named exactly like the class except
             // u don't use the world class
 
-
     int age;
 
     public Dog(int dogsAge){ //Add PARAMETERS to constructor
         age = dogsAge;
     }
 
-    //1. END: OF CONSTRUCTOR
 
 
-
-    //2. CREATE A NEW METHOD TO USE IN MAIN METHOD
+    //2a. CREATE A METHOD TO USE IN MAIN METHOD
 
     public void walkDog(){
         System.out.println("My dog cannot walk as he is " + age + " years old");
     }
 
 
-    //3. CREATE A NEW METHOD TO USE IN MAIN METHOD
+    //2b. CREATE A METHOD TO USE IN MAIN METHOD
     public void bark(){
         System.out.println("My dog cannot bark. He is " + age + " years old");
     }
 
 
+    //2c. CREATE A METHOD WITH PARAMETERS, TO USE IN MAIN METHOD
+    public void drive(int distanceInMiles){
+            System.out.println("I drove " + distanceInMiles + " miles with Binny my " +
+                    "old, lame dog");
+        }
 
+    //2d. CREATE A METHOD WITH A RETURN
+    public int numberOfLegs(){
+        return 4;
+    }
 
 
     //3.MAIN METHOD
@@ -41,7 +47,9 @@ public class Dog {
         Dog oldDog = new Dog(21);
         oldDog.walkDog();
         oldDog.bark();
-
+        oldDog.drive(10000);
+        int Legs = oldDog.numberOfLegs();
+        System.out.println(Legs);
     }
 
 }
