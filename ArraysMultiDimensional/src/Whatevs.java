849e3b2f390131151d51bd1c591b72251426b509
faithfem <fmugure@intracitygeeks.org>
2018-03-21 21:30:08 -0400

    Arrays-Tables

---
+++ ArraysMultiDimensional/src/Whatevs.java
@@ -0,0 +1,18 @@
+public class Whatevs {
+
+    public static void main(String[]args){
+
+        int firstArray[][]={{1,2,5,9,7}, {5,3,14,9,7}};
+        int secondArray[][]={{15,35,99,75,78}, {19,32,61,43}};
+
+    }
+
+    public static void display(int x[][]){
+            for(int row=0;row<x.length;row++){
+                for(int column=0;column<x[row].length;column++){
+
+                }
+                
+            }
+            }
+}
