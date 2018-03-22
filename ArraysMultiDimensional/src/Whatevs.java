d00ca3146fca5ead1e7b06c4e331c9a5df4d24ee
faithfem <fmugure@intracitygeeks.org>
2018-03-21 23:46:19 -0400

    Arrays-Tables

--- ArraysMultiDimensional/src/Whatevs.java
+++ ArraysMultiDimensional/src/Whatevs.java
@@ -2,17 +2,29 @@ public class Whatevs {
 
     public static void main(String[]args){
 
-        int firstArray[][]={{1,2,5,9,7}, {5,3,14,9,7}};
-        int secondArray[][]={{15,35,99,75,78}, {19,32,61,43}};
+        int firstArray[][]={{1,2,5,9,7}, {5,3,14,9,7}}; //first part is the row, second part is the column
+        int secondArray[][]={{15,35,99,75,78}, {19,32,61,43,25},{44,57}};
+
+        //Call the method below
+        System.out.println("This is the first array");
+        display(firstArray);
+
+        System.out.println("This is the second array");
+        display(secondArray);
 
     }
-
-    public static void display(int x[][]){
-            for(int row=0;row<x.length;row++){
-                for(int column=0;column<x[row].length;column++){
-
+    //Method to print the arrays
+    //Loop through the row
+    //Loop through the column
+    //Print rows and columns
+    //Create rows in separate lines
+    public static void display(int x[][]){ //create the method (called display)
+            for(int row=0;row<x.length;row++){ //creates the Row
+                for(int column=0;column<x[row].length;column++){ //creates the Column
+                    System.out.print(x [row][column] + "\t"); //prints the Row
                 }
-                
+                System.out.println(); //new line to indicate, print to separate rows
+
             }
             }
 }
